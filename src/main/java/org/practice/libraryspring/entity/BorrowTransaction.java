b2b80407fee5f077315dbf49dc438d85869fa097
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-11-23 16:04:17 +0700

    [FEATURE] Add BorrowTransaction attribute

---
+++ src/main/java/org/practice/libraryspring/entity/BorrowTransaction.java
@@ -0,0 +1,51 @@
+package org.practice.libraryspring.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Setter
+@Getter
+public class BorrowTransaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member  member;
+
+    @Column(nullable = false, name = "borrow_date")
+    private LocalDate borrowDate;
+
+    @Column(nullable = false, name = "due_date")
+    private LocalDate dueDate;
+
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+    private Double fine;
+
+    @PrePersist
+    public void prePersist() {
+        borrowDate = LocalDate.now();
+        dueDate = LocalDate.now().plusDays(7);
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        returnDate = LocalDate.now();
+        if(returnDate.isAfter(dueDate)) {
+            fine = (double) 1000 * (returnDate.getDayOfYear() - dueDate.getDayOfYear());
+        }
+    }
+}
