495aa01bdd8ba62fada547bfa770962896f721fc
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-11-24 09:15:25 +0700

    [FIX] change toAuthorResponse method and change transactional annotation from jakarta transaction to spring transaction

--- src/main/java/org/practice/libraryspring/service/impl/AuthorServiceImpl.java
+++ src/main/java/org/practice/libraryspring/service/impl/AuthorServiceImpl.java
@@ -1,6 +1,6 @@
 package org.practice.libraryspring.service.impl;
 
-import jakarta.transaction.Transactional;
+
 import lombok.RequiredArgsConstructor;
 import org.practice.libraryspring.dto.request.AuthorRequest;
 import org.practice.libraryspring.dto.response.AuthorResponse;
@@ -8,6 +8,7 @@ import org.practice.libraryspring.entity.Author;
 import org.practice.libraryspring.repository.AuthorRepository;
 import org.practice.libraryspring.service.AuthorService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,13 +26,16 @@ public class AuthorServiceImpl implements AuthorService {
                 .lastName(authorRequest.getLastName())
                 .build();
         authorRepository.save(author);
-        return toAuthorResponse(author);
+        return AuthorResponse.AuthorToAuthorResponse(author);
     }
 
     @Override
     public AuthorResponse updateAuthor(String id, AuthorRequest authorRequest) {
         Author author = getAuthorById(id);
-        return toAuthorResponse(authorRepository.save(author));
+        author.setFirstName(authorRequest.getFirstName());
+        author.setLastName(authorRequest.getLastName());
+        authorRepository.save(author);
+        return AuthorResponse.AuthorToAuthorResponse(author);
     }
 
     @Override
@@ -43,19 +47,12 @@ public class AuthorServiceImpl implements AuthorService {
     public List<AuthorResponse> getAllAuthors() {
         List<Author> authors = authorRepository.findAll();
         List<AuthorResponse> authorResponses = new ArrayList<>();
-        authors.forEach(author -> authorResponses.add(toAuthorResponse(author)));
+        authors.forEach(author -> authorResponses.add(AuthorResponse.AuthorToAuthorResponse(author));
         return authorResponses;
     }
 
     @Override
     public void deleteAuthor(String id) {
         authorRepository.deleteById(id);
-    }
-
-    public AuthorResponse toAuthorResponse(Author author)  {
-        AuthorResponse authorResponse = new AuthorResponse();
-        authorResponse.setId(author.getId());
-        authorResponse.setName(author.getFirstName() + " " + author.getLastName());
-        return authorResponse;
     }
 }
