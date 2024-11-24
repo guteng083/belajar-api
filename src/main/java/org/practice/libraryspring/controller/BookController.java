56e13cbf6802ff341c8dbc8e081eec723a0f7ff1
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-11-25 06:18:41 +0700

    [FEATURE] Add book controller

---
+++ src/main/java/org/practice/libraryspring/controller/BookController.java
@@ -0,0 +1,51 @@
+package org.practice.libraryspring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.practice.libraryspring.dto.request.BookRequest;
+import org.practice.libraryspring.dto.response.BookResponse;
+import org.practice.libraryspring.service.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/book")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+
+    @PostMapping("/create")
+    public ResponseEntity<?> addBook(@RequestBody BookRequest bookRequest) {
+        BookResponse bookResponse = bookService.addBook(bookRequest);
+        return ResponseEntity.ok(bookResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getBookById(@PathVariable Long id) {
+        return ResponseEntity.ok(bookService.getBookById(id));
+    }
+
+    @GetMapping("/{authorId}")
+    public ResponseEntity<?> getBookByAuthorId(@PathVariable Long authorId) {
+        List<BookResponse> bookResponses = bookService.getAllBooksByAuthorId(authorId);
+        return ResponseEntity.ok(bookResponses);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateBook(@PathVariable Long id, @RequestBody BookRequest bookRequest) {
+        BookResponse bookResponse = bookService.updateBook(id, bookRequest);
+        return ResponseEntity.ok(bookResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBook(@PathVariable Long id) {
+        bookService.deleteBookById(id);
+        return ResponseEntity.ok().build();
+    }
+}
