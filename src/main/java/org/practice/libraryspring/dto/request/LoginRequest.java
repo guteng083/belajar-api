6657d6f5109c356f479a5fe75bb738da85f99a08
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-11-30 14:10:20 +0700

    [FEATURE] add annotation

--- src/main/java/org/practice/libraryspring/dto/request/LoginRequest.java
+++ src/main/java/org/practice/libraryspring/dto/request/LoginRequest.java
@@ -1,5 +1,12 @@
 package org.practice.libraryspring.dto.request;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginRequest {
     private String username;
     private String password;
