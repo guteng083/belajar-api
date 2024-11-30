0eadbfd7a7394c0c69700fb454b668ff71f2319f
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-11-30 10:23:17 +0700

    [FIX] extend user service to userDetailsService

--- src/main/java/org/practice/libraryspring/service/UserService.java
+++ src/main/java/org/practice/libraryspring/service/UserService.java
@@ -1,8 +1,9 @@
 package org.practice.libraryspring.service;
 
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 }
