3df4529855fec8ef57475721518f7e1b68bb2929
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-11-30 14:06:50 +0700

    [FEATURE] add annotation

--- src/main/java/org/practice/libraryspring/dto/response/RegisterResponse.java
+++ src/main/java/org/practice/libraryspring/dto/response/RegisterResponse.java
@@ -1,5 +1,12 @@
 package org.practice.libraryspring.dto.response;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class RegisterResponse {
     private String username;
     private String role;
