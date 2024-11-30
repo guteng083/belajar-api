70dc68cdada7d8714c4a7385ac1adfa53331c311
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-11-30 14:51:39 +0700

    [FEATURE] Add authController for login and register

---
+++ src/main/java/org/practice/libraryspring/controller/AuthController.java
@@ -0,0 +1,38 @@
+package org.practice.libraryspring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.practice.libraryspring.dto.request.LoginRequest;
+import org.practice.libraryspring.dto.request.RegisterRequest;
+import org.practice.libraryspring.dto.response.LoginResponse;
+import org.practice.libraryspring.dto.response.RegisterResponse;
+import org.practice.libraryspring.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/register/librarian")
+    public ResponseEntity<?> register(RegisterRequest registerRequest) {
+        RegisterResponse registerResponse = authService.register(registerRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registerResponse);
+    }
+
+    @PostMapping("/register/admin")
+    public ResponseEntity<?> registerAdmin(RegisterRequest registerRequest) {
+        RegisterResponse registerResponse = authService.registerAdmin(registerRequest);
+        return ResponseEntity.status(HttpStatus.CREATED).body(registerResponse);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(LoginRequest loginRequest) {
+        LoginResponse loginResponse = authService.login(loginRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(loginResponse);
+    }
+}
