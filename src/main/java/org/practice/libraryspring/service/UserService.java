80251f7947825b792bb085aaf650792cecab3b4d
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-11-30 10:24:56 +0700

    [FIX] delete access identifier

--- src/main/java/org/practice/libraryspring/service/UserService.java
+++ src/main/java/org/practice/libraryspring/service/UserService.java
@@ -5,5 +5,5 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public interface UserService extends UserDetailsService {
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 }
