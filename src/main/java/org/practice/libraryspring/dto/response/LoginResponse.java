c5910fc2351e3425ac9d136ecce318ce0360be7e
Stefanus Arlian Situmeang <stevanusarlian@gmail.com>
2024-11-30 14:06:31 +0700

    [FEATURE] add annotation

--- src/main/java/org/practice/libraryspring/dto/response/LoginResponse.java
+++ src/main/java/org/practice/libraryspring/dto/response/LoginResponse.java
@@ -1,5 +1,12 @@
 package org.practice.libraryspring.dto.response;
 
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LoginResponse {
     private String username;
     private String role;
